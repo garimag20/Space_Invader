26daed6180a693d6fd71c5161c769b1f771cdf3c
garimag20 <garimagarg347@gmail.com>
2021-08-03 19:21:48 +0530

    Changed Title, Icon and background color

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ SCREENWIDTH = 600
 SCREENHEIGHT = 800
 SCREEN = pygame.display.set_mode((SCREENHEIGHT, SCREENWIDTH))
 
+#Title and icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
 #Game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    #changing background color-RGB
+    SCREEN.fill((0,0,0))
+    pygame.display.update()
