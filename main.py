facbe575f8e6fd6ca53ffb3b6ab4464dbe61ea1c
garimag20 <garimagarg347@gmail.com>
2021-08-03 21:47:21 +0530

    Added player

--- main.py
+++ main.py
@@ -4,21 +4,33 @@ import pygame
 pygame.init()
 
 #create the screen
-SCREENWIDTH = 600
-SCREENHEIGHT = 800
-SCREEN = pygame.display.set_mode((SCREENHEIGHT, SCREENWIDTH))
+SCREENWIDTH = 800
+SCREENHEIGHT = 600
+SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 
 #Title and icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
+#Player
+playerImg = pygame.image.load('player.png')
+playerX = 350
+playerY = 480
+
+def player():
+    SCREEN.blit(playerImg, (playerX, playerY))
+
+
 #Game loop
 running = True
 while running:
+    #changing background color-RGB
+    SCREEN.fill((0,0,0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    #changing background color-RGB
-    SCREEN.fill((0,0,0))
+
+    player()
     pygame.display.update()
