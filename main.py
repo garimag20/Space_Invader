cc24fde98a536f467c157d6145d83eaf734b43b7
garimag20 <garimagarg347@gmail.com>
2021-08-04 14:38:15 +0530

    Added enemy and randomized it's initial location

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pygame
-
-#initialize the pygame
+import random
+# initialize the pygame
 pygame.init()
 
-#create the screen
+# create the screen
 SCREENWIDTH = 800
 SCREENHEIGHT = 600
 SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
@@ -13,33 +13,44 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
-#Player
+# Player
 playerImg = pygame.image.load('player.png')
 playerX = 360
 playerY = 480
 playerX_change = 0
 
-def player(x,y):
+# Enemy
+enemyImg = pygame.image.load('enemy.png')
+enemyX = random.randint(0, 736)
+enemyY = random.randint(50, 150)
+enemyX_change = 0
+
+
+def enemy(x, y):
+    SCREEN.blit(enemyImg, (x, y))
+
+
+def player(x, y):
     SCREEN.blit(playerImg, (x, y))
 
 
-#Game loop
+# Game loop
 running = True
 while running:
-    #changing background color-RGB
-    SCREEN.fill((0,0,0))
+    # changing background color-RGB
+    SCREEN.fill((0, 0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        #if keystroke pressed is left or right arrow key
+        # if keystroke pressed is left or right arrow key
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 playerX_change = -0.3
             if event.key == pygame.K_RIGHT:
                 playerX_change = 0.3
-        
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
@@ -47,8 +58,9 @@ while running:
     playerX += playerX_change
     if playerX <= 0:
         playerX = 0
-    
-    elif playerX >= 736: #800-64 ->64 - size of spaceship
+
+    elif playerX >= 736:  # 800-64 ->64 - size of spaceship
         playerX = 736
     player(playerX, playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
