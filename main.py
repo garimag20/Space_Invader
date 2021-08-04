269d2644f8efaad8415ec36df092f2fa98b6976a
garimag20 <garimagarg347@gmail.com>
2021-08-04 12:40:15 +0530

    Added boundaries

--- main.py
+++ main.py
@@ -45,5 +45,10 @@ while running:
                 playerX_change = 0
 
     playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    
+    elif playerX >= 736: #800-64 ->64 - size of spaceship
+        playerX = 736
     player(playerX, playerY)
     pygame.display.update()
