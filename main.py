ad595ccd89d98d95cfabe6ed76a21b30c531ab0a
garimag20 <garimagarg347@gmail.com>
2021-08-04 12:33:50 +0530

    Added keyboard controls

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ pygame.display.set_icon(icon)
 
 #Player
 playerImg = pygame.image.load('player.png')
-playerX = 350
+playerX = 360
 playerY = 480
+playerX_change = 0
 
-def player():
-    SCREEN.blit(playerImg, (playerX, playerY))
+def player(x,y):
+    SCREEN.blit(playerImg, (x, y))
 
 
 #Game loop
@@ -32,5 +33,17 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    player()
+        #if keystroke pressed is left or right arrow key
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+    player(playerX, playerY)
     pygame.display.update()
