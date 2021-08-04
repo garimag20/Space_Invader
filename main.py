0649b8cba53187344160da100d6ba5eb98a43215
garimag20 <garimagarg347@gmail.com>
2021-08-04 15:09:44 +0530

    Enemy's movement mechanics

--- main.py
+++ main.py
@@ -21,9 +21,11 @@ playerX_change = 0
 
 # Enemy
 enemyImg = pygame.image.load('enemy.png')
-enemyX = random.randint(0, 736)
+enemyX = random.randint(0, 800)
 enemyY = random.randint(50, 150)
-enemyX_change = 0
+enemyX_change = 0.3
+enemyY_change = 40 
+
 
 
 def enemy(x, y):
@@ -55,12 +57,21 @@ while running:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
+    #checking for boundaries such that spaceship doesn't go out of bounds
     playerX += playerX_change
     if playerX <= 0:
         playerX = 0
-
     elif playerX >= 736:  # 800-64 ->64 - size of spaceship
         playerX = 736
+    
+    #enemy 
+    enemyX += enemyX_change
+    if enemyX <= 0:
+        enemyX_change = 0.3
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -0.3
+        enemyY += enemyY_change
     player(playerX, playerY)
     enemy(enemyX, enemyY)
     pygame.display.update()
